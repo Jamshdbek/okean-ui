5daa32c932acf85f8ca9a644f5e73f5599644dbc
jamshidbekd664@gmail.com <jamshidbekd664@gmail.com>
2024-10-16 15:16:33 +0500

    clearing project

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Button, Input } from "./components";
 function App() {
   return (
     <>
-      <Button className="text-black border" onClick={() => alert("hi")}>Hi</Button>
-      <Input className=" border-inherit border-blue-400" />
-      <h1>Okean UI</h1>
+      <h1>Okean UI 🌊</h1>
+      <Button className="text-black border" onClick={() => alert("hi")}>
+        Hi I'm Button
+      </Button>
+      <Input placeholder="hi I'm input" className=" border-inherit border-blue-400" />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
